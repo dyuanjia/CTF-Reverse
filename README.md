1f0176ce41ea3f8edb6abc2e3838b7be062f58c4
dyuanjia <yuanjia_dong@mymail.sutd.edu.sg>
2020-01-26 01:22:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CTF Reverse Engineering
+
+Some questions I did.
+
+---
+
+## picoCTF 2019 (10/2019)
+
+### asm4
+
+- assembly logic tracing
+- compile assembly with ([C](https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html))
---
+++ asm4/sol.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int asm4(char* in)
+{
+    int val;
+
+    asm (
+        "nop;"
+        "nop;"
+        "nop;"
+        //"push   ebp;"
+        //"mov    ebp,esp;"
+        "push   ebx;"
+        "sub    esp,0x10;"
+        "mov    DWORD PTR [ebp-0x10],0x276;"
+        "mov    DWORD PTR [ebp-0xc],0x0;"
+        "jmp    _asm_27;"
+    "_asm_23:"
+        "add    DWORD PTR [ebp-0xc],0x1;"
+    "_asm_27:"
+        "mov    edx,DWORD PTR [ebp-0xc];"
+        "mov    eax,DWORD PTR [%[pInput]];"
+        "add    eax,edx;"
+        "movzx  eax,BYTE PTR [eax];"
+        "test   al,al;"
+        "jne    _asm_23;"
+        "mov    DWORD PTR [ebp-0x8],0x1;"
+        "jmp    _asm_138;"
+    "_asm_51:"
+        "mov    edx,DWORD PTR [ebp-0x8];"
+        "mov    eax,DWORD PTR [%[pInput]];"
+        "add    eax,edx;"
+        "movzx  eax,BYTE PTR [eax];"
+        "movsx  edx,al;"
+        "mov    eax,DWORD PTR [ebp-0x8];"
+        "lea    ecx,[eax-0x1];"
+        "mov    eax,DWORD PTR [%[pInput]];"
+        "add    eax,ecx;"
+        "movzx  eax,BYTE PTR [eax];"
+        "movsx  eax,al;"
+        "sub    edx,eax;"
+        "mov    eax,edx;"
+        "mov    edx,eax;"
+        "mov    eax,DWORD PTR [ebp-0x10];"
+        "lea    ebx,[edx+eax*1];"
+        "mov    eax,DWORD PTR [ebp-0x8];"
+        "lea    edx,[eax+0x1];"
+        "mov    eax,DWORD PTR [%[pInput]];"
+        "add    eax,edx;"
+        "movzx  eax,BYTE PTR [eax];"
+        "movsx  edx,al;"
+        "mov    ecx,DWORD PTR [ebp-0x8];"
+        "mov    eax,DWORD PTR [%[pInput]];"
+        "add    eax,ecx;"
+        "movzx  eax,BYTE PTR [eax];"
+        "movsx  eax,al;"
+        "sub    edx,eax;"
+        "mov    eax,edx;"
+        "add    eax,ebx;"
+        "mov    DWORD PTR [ebp-0x10],eax;"
+        "add    DWORD PTR [ebp-0x8],0x1;"
+    "_asm_138:"
+        "mov    eax,DWORD PTR [ebp-0xc];"
+        "sub    eax,0x1;"
+        "cmp    DWORD PTR [ebp-0x8],eax;"
+        "jl     _asm_51;"
+        "mov    eax,DWORD PTR [ebp-0x10];"
+        "add    esp,0x10;"
+        "pop    ebx;"
+        //"pop    ebp;"
+        //"ret    ;"
+        "nop;"
+        "nop;"
+        "nop;"
+            :"=r"(val)
+            : [pInput] "m"(in)
+    );
+
+    return val;
+}
+
+int main(int argc, char** argv)
+{
+    printf("0x%x\n", asm4("picoCTF_75806"));
+
+    return 0;
+}
+
+// Jumps were ported to use labels
+// Function prolog and epilog were taken care of by the compiler and therefore commented out
+// The `nop`s were inserted in order to make it easier to locate the inline assembly with a debugger or disassembler.
